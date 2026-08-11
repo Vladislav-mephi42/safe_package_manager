b51b65c6ceff7c0c728c27753acfc941263b6015
vlad <vklyanchenko09@gmail.com>
2026-08-11 21:09:32 +0300

    change src/controler/controler.h

--- src/controler/controler.h
+++ src/controler/controler.h
@@ -16,27 +16,13 @@ private:
   std::vector<std::string> json_repozitories_names;
   std::string storage_file_name;
   Package_manager *pm = nullptr;
-  void correct_json(const std::string &file_name) {
-
-    std::ifstream file(file_name);
-    if (!file.is_open()) {
-      throw std::runtime_error("bad json file");
-    }
-    json data;
-    try {
-      file >> data;
-    } catch (...) {
-      throw std::runtime_error("parsing error");
-    }
-    if (!data.contains("packages")) {
-      throw std::runtime_error("bad json file");
-    }
-    if (!data["packages"].is_array()) {
-      throw std::runtime_error("bad json file");
-    }
-  }
+  void correct_json(const std::string &file_name);
 
 public:
+  /**
+   * @brief Constructs a new Controler object with default attributes.
+   *
+   */
   Controler() : storage_file_name("default.json") {
     Empty_with_main_read empty;
     Support_read support;
@@ -51,6 +37,14 @@ public:
     empty_read.push_des_strategies(des_strategies);
     read_strategies.push_back(std::make_shared<Empty_read>(empty_read));
   }
+  /**
+   * @brief Constructs a new Controler object with specified attributes.
+   *
+   * @param json_repositories_names
+   * @param storage_file_name
+   * @param pm
+   * @param load_from_storage
+   */
   Controler(std::vector<std::string> json_repositories_names,
             std::string storage_file_name, Package_manager *pm,
             bool load_from_storage = false)
@@ -72,29 +66,15 @@ public:
     read_strategies.push_back(std::make_shared<Default_read>(read));
 
     if (!load_from_storage) {
-      write_package_manager_to_file(storage_file_name, *pm);
+      write_package_manager_to_file(storage_file_name);
     } else {
-      read_package_manager_from_file(storage_file_name, *pm);
+      read_package_manager_from_file(storage_file_name);
     }
   }
-  ~Controler() = default;
-  Controler(const Controler &other)
-      : des_strategies(other.des_strategies),
-        read_strategies(other.read_strategies),
-        json_repozitories_names(other.json_repozitories_names),
-        storage_file_name(other.storage_file_name), pm(other.pm) {}
-
-  Controler &operator=(const Controler &other) {
-    if (this != &other) {
-      // Копируем все члены
-      des_strategies = other.des_strategies;
-      read_strategies = other.read_strategies;
-      json_repozitories_names = other.json_repozitories_names;
-      storage_file_name = other.storage_file_name;
-      pm = other.pm;
-    }
-    return *this;
-  }
+  /**
+   * @brief Destroy the Controler object
+   *
+   */
 
   Controler(const std::string &storage_file_name, Package_manager *pm)
       : storage_file_name(storage_file_name), pm(pm) {
@@ -108,65 +88,123 @@ public:
     Default_read read;
     read.push_des_strategies(des_strategies);
     read_strategies.push_back(std::make_shared<Default_read>(read));
-    write_package_manager_to_file(storage_file_name, *pm);
+    write_package_manager_to_file(storage_file_name);
   }
-
+  /**
+   * @brief Gets the json repozitories names.
+   *
+   * @return const std::vector<std::string>&
+   */
   const std::vector<std::string> &get_json_repozitories_names() const noexcept {
     return json_repozitories_names;
   }
+  /**
+   * @brief Gets the storage file name.
+   *
+   * @return const std::string&
+   */
   const std::string &get_storage_file_name() const noexcept {
     return storage_file_name;
   }
+  /**
+   * @brief Gets the strategies.
+   *
+   * @return const std::vector<std::shared_ptr<Deserialization_strategy>>&
+   */
   const std::vector<std::shared_ptr<Deserialization_strategy>> &
   get_strategies() const noexcept {
     return des_strategies;
   }
-
+  /**
+   * @brief Sets the json repozitories names.
+   *
+   * @param new_repozitories
+   */
   void set_json_repozitories_names(
       const std::vector<std::string> &new_repozitories) {
     json_repozitories_names = new_repozitories;
   }
+  /**
+   * @brief Sets the storage file name object.
+   *
+   * @param new_storage_file_name
+   */
   void set_storage_file_name(const std::string &new_storage_file_name) {
     storage_file_name = new_storage_file_name;
   }
+  /**
+   * @brief Sets the strategies.
+   *
+   * @param new_strategies
+   */
   void
   set_strategies(const std::vector<std::shared_ptr<Deserialization_strategy>>
                      new_strategies) {
     des_strategies = new_strategies;
   }
 
-  std::shared_ptr<Package>
-  read_package_from_file(const std::string &file_name,
-                         const std::string &input_file_name);
-  void read_package_manager_from_file(const std::string &input_file_name,
-                                      Package_manager &pm);
-  void write_package_to_file(const std::shared_ptr<Package> &package,
-                             const std::string &output_file_name);
-  void write_package_manager_to_file(const std::string &output_file_name,
-                                     Package_manager &pm);
+  /**
+   * @brief  Open the input json file and deserialize package manager object
+   * from it.
+   *
+   * @param input_file_name
+   */
+  void read_package_manager_from_file(const std::string &input_file_name);
 
+  /**
+   * @brief Serealizes the input package manager object into json string, than
+   * opens file with given output file name and writes serialized package
+   * manager into the file.
+   *
+   * @param output_file_name
+   */
+  void write_package_manager_to_file(const std::string &output_file_name);
+  /**
+   * @brief Find package with given file name in json data and deserializes it
+   * using special strategies methods.
+   *
+   * @param file_name
+   * @param data
+   * @return std::shared_ptr<Package>
+   */
   std::shared_ptr<Package> read_package(const std::string &file_name,
                                         json &data) const;
-  std::shared_ptr<Package>
-  read_package(const std::string &file_name, json &data,
-               std::vector<std::string> &added_packages) const;
 
-  static std::ostream &write_package(const std::shared_ptr<Package> &package,
-                                     std::ostream &out);
-
-  json find_package(std::istream &in, const std::string &file_name);
-  json find_package(json &data, const std::string &file_name);
-  json find_package(const std::string &filename, const std::string &file_name);
-
+  /**
+   * @brief Find the serialized package in controler repozitpries, than read it
+   * and add to the package manager
+   *
+   * @param file_name
+   */
   void add_package(const std::string &file_name);
+  /**
+   * @brief Adds package from external source.
+   *
+   * @param package
+   */
   void add_package_external(const std::shared_ptr<Package> &package);
+  /**
+   * @brief Removes the package from package manager. After that stores it to
+   * the storadge.
+   *
+   * @param file_name
+   */
   void remove_package(const std::string &file_name);
+  /**
+   * @brief Finds the package in package manager.
+   *
+   * @param file_name
+   * @return true
+   * @return false
+   */
   bool find_package(const std::string &file_name) {
     return pm->find(file_name);
   }
-  void write_package_to_json(const std::shared_ptr<Package> &package,
-                             json &data);
+  std::shared_ptr<Package>
+  read_package_from_file(const std::string &file_name,
+                         const std::string &input_file_name);
   void remove_unuse();
   void global_update();
 };
+
 #endif
